3f61419479572733204e43c80be08183313a57bc
Kunal <kunalraj1705@gmail.com>
2026-08-17 07:29:35 +0530

    gitignore

--- src/main/java/com/krb/enterprise/user/infrastructure/security/JwtConfiguration.java
+++ src/main/java/com/krb/enterprise/user/infrastructure/security/JwtConfiguration.java
@@ -1,5 +1,15 @@
 package com.krb.enterprise.user.infrastructure.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+
+@Configuration
 public class JwtConfiguration {
 
+    @Bean
+    public JwtEncoder jwtEncoder() {
+        return null;
+    }
 }
