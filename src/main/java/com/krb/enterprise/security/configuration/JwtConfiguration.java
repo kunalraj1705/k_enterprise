3e26cacb99955714e7931f3456f591978a10537c
Kunal <kunalraj1705@gmail.com>
2026-08-18 08:47:55 +0530

    Restructure

--- src/main/java/com/krb/enterprise/security/configuration/JwtConfiguration.java
+++ src/main/java/com/krb/enterprise/security/configuration/JwtConfiguration.java
@@ -5,7 +5,9 @@ import java.security.interfaces.RSAPublicKey;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 
 import com.nimbusds.jose.jwk.JWKSet;
@@ -23,5 +25,10 @@ public class JwtConfiguration {
                 .build();
 
         return new NimbusJwtEncoder(new ImmutableJWKSet<>(new JWKSet(rsaKey)));
+    }
+
+       @Bean
+    public JwtDecoder jwtDecoder(RSAPublicKey publicKey) {
+        return NimbusJwtDecoder.withPublicKey(publicKey).build();
     }
 }
